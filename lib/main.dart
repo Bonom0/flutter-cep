630cfb71a09bc972c5cf0ea8d60c4c428908e191
Bonom0 <caioazbonomo@gmail.com>
2025-08-14 23:38:37 -0300

    Criado projeto e criado estrutura de pastas

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterCEPApp());
+}
+
+class FlutterCEPApp extends StatelessWidget {
+  const FlutterCEPApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consulta de CEP',
+      home: Container(),
+    );
+  }
+}
